dfcf8037a86ad308c58d3fe6b2841b7a57dc20ce
JaimeSerrano15 <00025318@uca.edu.sv>
2019-10-13 16:53:33 -0600

    [Ejer3JS]Add: Terminando HTML, CSS y comenzando JS

---
+++ blog/JS/main.js
@@ -0,0 +1,20 @@
+window.onload = function(){
+    var days = document.querySelectorAll("span");
+    //console.log(days);
+    days[0].addEventListener("click", ()=>{
+        for(let i=0; i<5;i++)
+        })
+    })
+    days[1].addEventListener("click", ()=>{
+        
+    })
+    days[2].addEventListener("click", ()=>{
+        
+    })
+    days[3].addEventListener("click", ()=>{
+        
+    })
+    days[4].addEventListener("click", ()=>{
+        
+    })
+}
