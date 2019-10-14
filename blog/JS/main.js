3675abf745ef9ee3b8395878dd95ac660da93145
JaimeSerrano15 <00025318@uca.edu.sv>
2019-10-13 23:05:13 -0600

    [Ejer3JS]Add: JS funcional, arreglando orden, agregando descripcion

--- blog/JS/main.js
+++ blog/JS/main.js
@@ -1,20 +1,38 @@
 window.onload = function(){
     var days = document.querySelectorAll("span");
-    //console.log(days);
-    days[0].addEventListener("click", ()=>{
-        for(let i=0; i<5;i++)
-        })
-    })
-    days[1].addEventListener("click", ()=>{
-        
-    })
-    days[2].addEventListener("click", ()=>{
-        
-    })
-    days[3].addEventListener("click", ()=>{
-        
-    })
-    days[4].addEventListener("click", ()=>{
-        
-    })
+    days[0].addEventListener("click",()=>{
+        document.querySelector("article.one").classList.toggle("off");
+        for(let i=0; i<5;i++){
+            if(i!=0)
+                days[i].classList.toggle("off");
+        }
+    });
+    days[1].addEventListener("click",()=>{
+        document.querySelector("article.two").classList.toggle("off");
+        for(let i=0; i<5;i++){
+            if(i!=1)
+                days[i].classList.toggle("off");
+        }
+    });
+    days[2].addEventListener("click",()=>{
+        document.querySelector("article.three").classList.toggle("off");
+        for(let i=0; i<5;i++){
+            if(i!=2)
+                days[i].classList.toggle("off");
+        }
+    });
+    days[3].addEventListener("click",()=>{
+        document.querySelector("article.four").classList.toggle("off");
+        for(let i=0; i<5;i++){
+            if(i!=3)
+                days[i].classList.toggle("off");
+        }
+    });
+    days[4].addEventListener("click",()=>{
+        document.querySelector("article.five").classList.toggle("off");
+        for(let i=0; i<5;i++){
+            if(i!=4)
+                days[i].classList.toggle("off");
+        }
+    });
 }
