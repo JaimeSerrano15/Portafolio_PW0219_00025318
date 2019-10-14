06667c6b4d453519bfd20d9190d5093a7d107280
JaimeSerrano15 <00025318@uca.edu.sv>
2019-10-14 15:35:24 -0600

    [Ejer3JS]Add: Finalizando JS

--- blog/JS/main.js
+++ blog/JS/main.js
@@ -1,38 +1,14 @@
 window.onload = function(){
-    var days = document.querySelectorAll("span");
-    days[0].addEventListener("click",()=>{
-        document.querySelector("article.one").classList.toggle("off");
-        for(let i=0; i<5;i++){
-            if(i!=0)
-                days[i].classList.toggle("off");
-        }
+    var days = document.querySelectorAll(".links");
+    var arts = document.querySelectorAll(".art");
+    days.forEach((item)=>{
+        item.addEventListener("click",()=>{
+            arts.forEach((item)=>{
+                item.classList.add("off");
+            });
+            var on = item.id;
+            var arton = document.querySelector(".art." + on);
+            arton.classList.toggle("off");
+        })
     });
-    days[1].addEventListener("click",()=>{
-        document.querySelector("article.two").classList.toggle("off");
-        for(let i=0; i<5;i++){
-            if(i!=1)
-                days[i].classList.toggle("off");
-        }
-    });
-    days[2].addEventListener("click",()=>{
-        document.querySelector("article.three").classList.toggle("off");
-        for(let i=0; i<5;i++){
-            if(i!=2)
-                days[i].classList.toggle("off");
-        }
-    });
-    days[3].addEventListener("click",()=>{
-        document.querySelector("article.four").classList.toggle("off");
-        for(let i=0; i<5;i++){
-            if(i!=3)
-                days[i].classList.toggle("off");
-        }
-    });
-    days[4].addEventListener("click",()=>{
-        document.querySelector("article.five").classList.toggle("off");
-        for(let i=0; i<5;i++){
-            if(i!=4)
-                days[i].classList.toggle("off");
-        }
-    });
-}
+};
