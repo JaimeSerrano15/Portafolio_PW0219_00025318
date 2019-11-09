23dd502e045ab53fe5fd71c2809d74eec6fc532a
JaimeSerrano15 <00025318@uca.edu.sv>
2019-11-08 21:57:45 -0600

    Finalizando Labo

--- Laboratorios/Laboratorio10/rest_api/app.js
+++ Laboratorios/Laboratorio10/rest_api/app.js
@@ -2,14 +2,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var database = require('./bin/database');
+var database = require('./config/database');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
-database.connectDB();
+database.connectDb();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -18,6 +17,5 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 module.exports = app;
--- Laboratorios/Laboratorio10/rest_api/routes/index.js
+++ Laboratorios/Laboratorio10/rest_api/routes/index.js
@@ -1,21 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var register = require("../models/register");
+var StudentManager = require('../controllers/StudentManager');
 
-/* GET home page. */
-router.get('/register', function(req, res, next) {
-  console.log(req);
-  register.find((err, register)=>{
-    if(err){
-      return Response.status(500).json({msj: "Error interno del sistema"});
-    }
-    if(register){
-      Response.status(200).json(register);
-    }
-    else{
-      Response.status(404).json({msj: "No hay registro"});
-    }
-  });
-});
+router.get('/', StudentManager.getAll);
+
+router.get('/:id', StudentManager.getStudentById);
+
+router.post('/create', StudentManager.create);
+
+router.put('/update', StudentManager.update);
+
+router.delete('/delete', StudentManager.delete);
 
 module.exports = router;
